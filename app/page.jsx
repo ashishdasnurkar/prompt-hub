e667ad0a51a1a0dcf0764f502ec6eedbaccda004
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-05-21 00:58:01 +1000

    update home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI Prompts</span>
+      </h1>
+      <p className="desc text-cetner">
+        Create and Share cool AI prompts with others.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
