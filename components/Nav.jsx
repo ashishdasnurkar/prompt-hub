604a6acbb743f0d1e6ac8330dd0a56990e6b69ab
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-05-24 21:47:08 +1000

    fixed nav html structure

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,71 +25,71 @@ const Nav = () => {
           className="object-contain"
         />
         <p className="logo-text">Prompt Hub</p>
+      </Link>
 
-        {/*  Desktop Navigation */}
-        <div className="sm:flex hidden">
-          {isUserLoggedIn ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link href="/create-post" className="black_btn">
-                Create Post
-              </Link>
-              <button type="button" className="outline_btn" onClick={signOut}>
-                Sign Out
-              </button>
-              <Link href="/profile">
-                <Image
-                  src="/assets/images/logo.svg"
-                  width={37}
-                  height={37}
-                  alt="profile"
-                />
-              </Link>
-            </div>
-          ) : (
-            <>
-              {providers &&
-                Object.values(providers).map((provider) => (
-                  <button
-                    type="button"
-                    key={provider.name``}
-                    onClick={() => signIn(provider.id)}
-                    className="black_btn"
-                  >
-                    Sign In
-                  </button>
-                ))}
-            </>
-          )}
-        </div>
-
-        {/*  Desktop Navigation */}
-        <div className="sm:hidden flex relative">
-          {isUserLoggedIn ? (
-            <div className="flex">
+      {/*  Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-post" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" className="outline_btn" onClick={signOut}>
+              Sign Out
+            </button>
+            <Link href="/profile">
               <Image
                 src="/assets/images/logo.svg"
                 width={37}
                 height={37}
                 alt="profile"
               />
-            </div>
-          ) : (
-            <>
-              {providers &&
-                Object.values(providers).map((provider) => (
-                  <button
-                    type="button"
-                    key={provider.name``}
-                    onClick={() => signIn(provider.id)}
-                    className="black_btn"
-                  >
-                    Sign In
-                  </button>
-                ))}
-            </>
-          )}
-        </div>
-      </Link>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name``}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/*  Desktop Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="profile"
+            />
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name``}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
