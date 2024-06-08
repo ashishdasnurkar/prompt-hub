a6125ce2896e8dbbc95e3a2c5808bdaa051d4632
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-06-09 09:12:55 +1000

    set up create prompt page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import { Form } from "@components/Form";
+
+const CreatePrompt = () => {
+  return <Form />;
+};
+
+export default CreatePrompt;
