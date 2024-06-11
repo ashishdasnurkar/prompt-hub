c9fb7c9ef668a6379f2d14ee711aca3e429bc81d
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-06-11 10:15:44 +1000

    init create prompt form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,12 +1,19 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import { Form } from "@components/Form";
 
 const CreatePrompt = () => {
-  return <Form />;
+  const [setsubmitting, setSetsubmitting] = useState(false);
+  const [Post, setPost] = useState({
+    post: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {};
+  return <Form type="Create" post={post} />;
 };
 
 export default CreatePrompt;
