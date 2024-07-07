645c6f6c09aabc03ac2ac3ddb954c0e7f6e49c2f
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-07-07 21:44:08 +1000

    update prompt page

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+import Form from "@components/Form";
+
+const UpdatePrompt = () => {
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default UpdatePrompt;
