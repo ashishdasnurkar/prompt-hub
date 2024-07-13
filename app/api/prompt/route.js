c47d4bff77b3d310d2f4f5c413d565dfd9004381
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2024-07-13 21:21:33 +1000

    impl upatch  prompt api route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -12,3 +12,28 @@ export const GET = async (request) => {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    // Find the existing prompt by ID
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    // Update the prompt with new data
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response("Successfully updated the Prompts", { status: 200 });
+  } catch (error) {
+    return new Response("Error Updating Prompt", { status: 500 });
+  }
+};
